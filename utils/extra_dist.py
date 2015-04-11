51a85e1c7aaa125a944faac382b0d7c73a6ebdf6
bougie <bougie@appartland.eu>
2015-04-11 19:03:54 +0200

    Improve extra_dist tools

--- utils/extra_dist.py
+++ utils/extra_dist.py
@@ -1,7 +1,56 @@
 # generate makefile extra_dist variables for fetching depends
-
+import re
 import sys
+from collections import OrderedDict
 
+
+def get_dist_name(filename):
+    try:
+        dist = re.compile('(-[0-9])').split('-'.join(filename))[0]
+    except:
+        dist = ''.join(filename[:len(filename) - 1])
+    finally:
+        for c in ['.', '-']:
+            dist = dist.replace(c, '')
+        return dist
+
+
+def get_suffix(filename):
+    try:
+        suffix = ''.join(re.compile('(-[0-9])').split('-'.join(filename))[1:])
+    except:
+        suffix = filename[-1]
+    finally:
+        if not suffix.startswith('-'):
+            suffix = '-' + suffix
+        return suffix
+
+
+def parse_lines(lines):
+    libs = {}
+
+    for line in lines:
+        if len(line.strip()) == 0:
+            continue
+
+        items = line.split('/')
+        filename = items[-1].split('-')
+
+        dist = get_dist_name(filename[:len(filename) - 1])
+        suffix = get_suffix(filename)
+        # realname = '-'.join(filename[:len(filename) - 1])
+        realname = '-'.join(filename).replace(suffix, '')
+
+        libs[dist] = OrderedDict()
+        if realname != dist:
+            libs[dist]['realname'] = realname
+        libs[dist]['suffix'] = suffix
+        libs[dist]['subdir'] = '/'.join(items[4:len(items) - 1])
+
+    _libs = OrderedDict()
+    for k in sorted(libs.keys()):
+        _libs[k] = libs[k]
+    return _libs
 
 if __name__ == "__main__":
     try:
@@ -10,24 +59,19 @@ if __name__ == "__main__":
     except:
         sys.exit(1)
     else:
-        extra_dist = []
-        for line in lines:
-            if len(line.strip()) == 0:
-                continue
+        libs = parse_lines(lines)
+        nb_cols = max([len(k) for k in libs.keys()])
 
-            items = line.split('/')
-            filename = items[-1].split('-')
+        print("EXTRA_DIST= %s\n" % (' '.join(sorted(libs.keys())),))
+        for dist, items in libs.items():
+            for item in items:
+                s = nb_cols - len(dist + "_" + item.upper())
+                if s > 20:
+                    s = 20
+                spaces = ' '
+                if s > 0:
+                    for x in range(s):
+                        spaces += ' '
 
-            dist = ''.join(filename[:len(filename) - 1]).replace('.', '')
-            suffix = filename[-1]
-            subdir = '/'.join(items[4:len(items) - 1])
-            realname = '-'.join(filename[:len(filename) - 1])
-
-            print("\n%s_SUFFIX=   -%s" % (dist, suffix))
-            print("%s_SUBDIR=   %s" % (dist, subdir))
-            if realname != dist:
-                print("%s_REALNAME= %s" % (dist, realname))
-
-            extra_dist.append(dist)
-
-        print("\nEXTRA_DIST= %s" % (' '.join(extra_dist),))
+                print("%s_%s=%s%s" % (dist, item.upper(), spaces, items[item]))
+            print("")
